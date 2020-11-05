03bed6abdf9e0eb314a71d391bdc3f4b556a43b2
Adam Selker <adam.selker@joby.aero>
2020-11-04 17:27:25 -0800

    Add cpu freq

--- mem.sh
+++ mem.sh
@@ -4,5 +4,5 @@ i3status | (read line && echo "$line" && read line && echo "$line" && read line 
 do
   read line
 	# memory= awk '/MemTotal/ {memtotal=$2}; /MemAvailable/ {memavail=$2}; END { printf("%.0f", memavail / 1000) }' /proc/meminfo;
- 	echo ",[{\"full_text\":\"$(awk '/MemTotal/ {memtotal=$2}; /MemAvailable/ {memavail=$2}; END { printf("%.0f", memavail / 1000) }' /proc/meminfo)\" },${line#,\[}" || exit 1
+	echo ",[{\"full_text\":\"$(cpupower frequency-info -f | sed -n '2s/\([^0-9]\|.\{3\} \)//gp')\"},{\"full_text\":\"$(awk '/MemTotal/ {memtotal=$2}; /MemAvailable/ {memavail=$2}; END { printf("%.0f", memavail / 1000) }' /proc/meminfo)\"},${line#,\[}" || exit 1
 done)
